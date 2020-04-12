587dd6c0fdbde016d7683ab97afcc8348e92c1ee
Tri Vo <tri511g@gmail.com>
2020-04-12 12:17:46 +1000

    Initial Commit

---
+++ ECG2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ECG2
+//
+//  Created by Tri Vo on 12/4/20.
+//  Copyright © 2020 Tri Vo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
